d8f18d7caefa87f3f9b97acbeb29c3dea8e6c2b4
Pulukuri-Mohan-Sai-Chandra <150537586+Pulukuri-Mohan-Sai-Chandra@users.noreply.github.com>
2024-08-05 00:38:42 +0530

    Completed till Passing the Props to the Compenets

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users'
+
+const random_index = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
-  selector: 'app-user',
+  selector: 'user_comp',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+  @Input()
+  user!: { id: string; name: string; avatar: string };
+  onClickUser() {
+    alert("To be Implemented")
+  }
+  get imgPath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
 
 }
