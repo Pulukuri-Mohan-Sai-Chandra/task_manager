0a11401f46a169cce0a38498808141a1911454e4
Pulukuri-Mohan-Sai-Chandra <150537586+Pulukuri-Mohan-Sai-Chandra@users.noreply.github.com>
2024-08-06 22:27:20 +0530

    Modified the Task ans Tasks Components

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,Output,EventEmitter} from '@angular/core';
 import {NgIf,NgFor} from '@angular/common'
 import {User,Task} from '../../Types_Interfaces/types_interfaces'
 @Component({
@@ -9,5 +9,8 @@ import {User,Task} from '../../Types_Interfaces/types_interfaces'
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-  @Input({required:true}) curr_tasks?:Task[]
+  @Input({required:true}) task?:Task
+  
+  @Output() currTask = new EventEmitter<Task>(); 
+
 }
